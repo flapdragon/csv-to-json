ce3f2f27a4d7b7b7c94f6c249fbaf398dd5ec476
David Dean <ddean@perseverenow.org>
2025-08-05 10:13:37 -0700

    Adds file reader, json builder and file writer to index.js. Updates gitignore rules for csvs and jsons.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello CSV")
+import fs from "fs"
+import { parse } from "fast-csv"
+
+const filePath = "cereal.csv"
+
+let csvArray = { data: []}
+let headers
+let index = 0
+
+fs.createReadStream(filePath)
+    .pipe(parse())
+    .on("error", error => console.error(error))
+    .on("data", (row) => {
+      if (index === 0) {
+        headers = [ ...row ]
+      }
+      else {
+        let rowObject = {}
+        for (let i = 0; i < row.length; i++) {
+          rowObject[headers[i]] = row[i]
+        }
+        csvArray.data.push(rowObject)
+      }
+      index++
+    }
+    )
+    .on("end", (rowCount) => {
+      console.log(`$${rowCount} charged to your credit card.`)
+      fs.writeFileSync(`${filePath.split(".")[0]}.json`, JSON.stringify(csvArray))
+    })
